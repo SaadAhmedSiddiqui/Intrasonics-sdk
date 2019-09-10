ffa4c199d4585d7b002d021f13e39e5e2a768748
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2019-09-10 15:53:52 +0500

    implemented encoder

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Infrasonics-nodejs-sdk
+# Intrasonics SDK
+
+Intrasonics SDK is a Nodejs wrapper sdk to assist you with command line encoding software of intrasonics
+
+  + The Intrasonics AV encoder allows users to insert codewords into media (audio/video) files. The user can specify which codewords they wish to insert and at what point in time within the media file they should be inserted.
+  + The encoder is available on Windows, Mac, or Linux. It uses a command/terminal window and the user may specify codewords and timings using either a configuration file or by typing commands at a prompt.
+  + Magic
+
+## Tech
+
+Intrasonics SDK uses a number of open source projects to work properly:
+
+* [npm](https://www.npmjs.com/) - Essential JavaScript development tools that help you go to market faster and build powerful applications using modern open source code.
+* [node.js](https://nodejs.org) - evented I/O for the backend
+* [intrasonics](https://www.intrasonics.com) - Intrasonics' audio watermarking technology uses sound to help you harness consumer interest when it matters most.
+
+## Requirements
+Intrasonics SDK uses resources provided by intrasonics:
+* [encoding software](https://portal.intrasonics.com/app/resources) - A Folder contains files AVFileDecoder ffmpeg ffprobe files provided by intrasonic for encoding
+* [encoder token](https://portal.intrasonics.com/app/tokens) - Encoder token (with .ist suffix) used to get access provided by intrasonics
+
+## Encoder Sofware Versions used
+* Linux - LinuxAVEncoder_4.2.0
+* Mac - OSXAVEncoder_4.2.0
+* Windows - WindowsAVEncoder_1.0.7
+
+## Installation
+``` sh
+npm install intrasonics-sdk
+```
+
+## Example
+``` sh
+const IntrasonicsSDK = require('intrasonics-sdk');
+
+let intrasonicsSDK;
+try {
+  intrasonicsSDK = new IntrasonicsSDK({ softwareFolder: 'intrasonics', tokenPath: 'intrasonics/intrasonics-token.ist' });
+  intrasonicsSDK.encode({
+    filePath: 'input-files/audio.mp3',
+    codeword: '10001',
+    outputPath: 'encoded-files/audio_encoded.mp3',
+    eventEngine: true
+  })
+    .then((stdout) => {
+      console.log(stdout);
+    }, (err) => {
+      console.error(err);
+    })
+} catch (err) {
+  console.error(err);
+}
+```
+
---
+++ index.js
@@ -0,0 +1,113 @@
+const { statSync } = require('fs');
+const { join, resolve: resolvePath } = require('path');
+const { exec } = require('child_process');
+
+const IntrasonicsSDK = (function () {
+  /**
+   * Represents Intrasonics SDK wrapper for nodejs.
+   * @constructor
+   * @param {object} config - Intrasonics configuration.
+   * @param {string} config.softwareFolder - name or path to folder which will contain AVFileDecoder ffmpeg ffprobe files i.e. Encoder Software.
+   * @param {string} config.tokenPath - name or path to encoder token (with .ist suffix).
+   * @property {function} encode
+   */
+  function IntrasonicsSDK(config) {
+    config = config || {};
+    if (!config.softwareFolder) {
+      throw new Error("Please include softwareFolder to folder which will contain AVFileDecoder ffmpeg ffprobe files");
+    }
+    if (!config.tokenPath || !(/\.ist$/).test(config.tokenPath)) {
+      throw new Error("Please include tokenPath path to Encoder token (with .ist suffix)");
+    }
+    config.softwareFolder = resolvePath(config.softwareFolder);
+    config.tokenPath = resolvePath(config.tokenPath);
+
+    let pathResult;
+    try {
+      pathResult = statSync(config.softwareFolder);
+    } catch (error) {
+      throw new Error(`softwareFolder ${config.softwareFolder} folder does not exist`);
+    }
+    if (!pathResult.isDirectory()) {
+      throw new Error(`softwareFolder ${config.softwareFolder} is invalid directory`);
+    }
+    if (!pathResult || !pathResult.isDirectory()) {
+      next(createError(400, `path ${watcherPath} is invalid directory`));
+      return
+    }
+    try {
+      statSync(join(config.softwareFolder, 'AVFileDecoder'));
+      statSync(join(config.softwareFolder, 'ffmpeg'));
+      statSync(join(config.softwareFolder, 'ffprobe'));
+    } catch (error) {
+      throw new Error(`${config.softwareFolder} foler must includes all AVFileDecoder ffmpeg ffprobe files`);
+    }
+    try {
+      pathResult = statSync(config.tokenPath);
+    } catch (error) {
+      throw new Error(`${config.tokenPath} does not exist`);
+    }
+
+    this.config = config;
+  }
+  /**
+   * Intrasonics AV encoder allows users to insert codewords into media (audio/video) files.
+   * @async
+   * @function encode
+   * @param {object} config - Intrasonics Encoder configuration.
+   * @param {string} config.filePath - name or Path of a audio/video file to be encoded.
+   * @param {string} config.codeword - watermarkId/trackId that will be encoded in audio/video file.
+   * @param {string} [config.outputPath] - file name or file path.
+   * @param {boolean | number} [config.eventEngine] - controls Event Engine 2 encoding true for on or time offset.
+   * @param {number} [config.time] - time in seconds where the codeword will be placed.
+   * @return {Promise<string>} - console output
+   */
+  IntrasonicsSDK.prototype.encode = async function Encode(config) {
+    return new Promise((resolve, reject) => {
+      config = config || {};
+      if (!config.filePath) {
+        throw new Error("Please include filePath to audio/video file that will be encoded");
+      }
+      if (!config.codeword) {
+        throw new Error("Please include codeword (watermarkid/trackId) that will be encoded in audio/video file");
+      }
+      config.filePath = resolvePath(config.filePath);
+
+      let execStr = `${this.config.softwareFolder}/AVFileEncoder `
+        + `${config.filePath} `
+        + `-l ${this.config.tokenPath} `
+        + `-c ${config.codeword} `;
+
+      if (config.outputPath) {
+        config.outputPath = resolvePath(config.outputPath);
+        console.log(config.outputPath);
+        execStr += `-o ${config.outputPath} `
+      }
+      if (config.time) {
+        execStr += `-t ${config.time}`;
+      }
+      if (config.eventEngine) {
+        if (config.eventEngine === true) {
+          execStr += '-e on';
+        } else {
+          execStr += `-e ${config.eventEngine}`;
+        }
+      }
+
+      console.log(execStr);
+      exec(execStr, async (err, stdout, stderr) => {
+        if (err) {
+          console.error(stderr);
+          reject(err);
+        }
+
+        resolve(stdout);
+      });
+    });
+  };
+
+  return IntrasonicsSDK;
+})();
+
+exports['default'] = IntrasonicsSDK;
+module.exports = exports['default'];
